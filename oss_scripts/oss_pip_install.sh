19ba18c8234d3f1d47cfebccff58496f8af70852
Ryan Sepassi <rsepassi@google.com>
2019-02-06 22:29:54 -0800

    verbose

--- oss_scripts/oss_pip_install.sh
+++ oss_scripts/oss_pip_install.sh
@@ -13,8 +13,8 @@ else
   echo "Installing ffmpeg"
   sudo add-apt-repository -y ppa:jonathonf/ffmpeg-4
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
-  sudo apt-get -qq update
-  sudo apt-get install -qq -y ffmpeg
+  sudo apt-get update
+  sudo apt-get install -y ffmpeg
 fi
 
 if [[ "$TF_VERSION" == "tf-nightly"  ]]
